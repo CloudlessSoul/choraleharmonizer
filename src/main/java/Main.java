b668502ad33aab1e1e925a09388a28a173d213a5
Alexander Dodd <alexanderdodd@Alexanders-MacBook-Pro.local>
2021-07-23 13:51:33 +0200

    Tidying up main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,19 +11,13 @@ public class Main {
 
         Pattern phrase1 = new Pattern("C6q G5q A5q G5i F5i | E5h G5h");
 
-        Pattern phrase2 = new Pattern("A4i G4i A4q G4q A4i B4i | C5h C5h");
+        Pattern phrase2 = new Pattern("A5i G5i A5q G5q A5i B5i | C6h C6h");
 
-        // now manually add the chords to see how that would work with JFugue
+        ChordProgression chordsForPhrase1 = new Harmonizer().generateChordProgressionFromPhrase(phrase1);
+        ChordProgression chordsForPhrase2 = new Harmonizer().generateChordProgressionFromPhrase(phrase2);
 
-        ChordProgression cp = new ChordProgression("I I^ IV V I")
-                .allChordsAs("$0q $1q $2q $3q $4w")
-                .setKey("C");
-
-        //player.play(phrase1.setVoice(0).add(cp.getPattern().setVoice(1)));
-
-        cp = new Harmonizer().generateChordProgressionFromPhrase(phrase2);
-
-        player.play(phrase2.setVoice(0).add(cp.getPattern().setVoice(1)));
-
+        Pattern phrase1WithHarmony = phrase1.setVoice(0).add(chordsForPhrase1.getPattern().setVoice(1));
+        Pattern phrase2WithHarmony = phrase2.setVoice(0).add(chordsForPhrase2.getPattern().setVoice(1));
+        player.play(phrase1WithHarmony, phrase2WithHarmony);
     }
 }
